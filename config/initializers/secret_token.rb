5922d9f5f7d63bc313267241580a365ab61051cb
brownie3003 <brownie3003@gmail.com>
2013-04-12 21:41:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TibDbApp::Application.config.secret_key_base = 'd88256c040f1fd05c0efc10f1f0451d6f2c74409495626f2371917db20d3b26bf0f7c381e9a7e1358a050be73bf1c6c7060d4cfb0f1027c0e87dda380c98cd63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TibDbApp::Application.config.session_store :encrypted_cookie_store, key: '_tib_db_app_session'
