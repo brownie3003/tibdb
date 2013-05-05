4b5fa1ad529425ff6e2073ffb012178e09cf82b5
brownie3003 <brownie3003@gmail.com>
2013-05-06 00:16:00 +0100

    Adding authorisation as much as possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-	def show
+	before_action :signed_in_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+
+    def show
 		@user = User.find(params[:id])
 	end
 
@@ -8,21 +11,50 @@ class UsersController < ApplicationController
 	end
 
 	def create
-    	@user = User.new(user_params)  
-    	if @user.save
-        sign_in @user
-        flash[:success] = "Welcome to the TiB Assignment Database"
-    		redirect_to @user
-    	else
-    		render 'new'
-    	end
-  	end
+    @user = User.new(user_params)  
+        if @user.save
+            sign_in @user
+            flash[:success] = "Welcome to the TiB Assignment Database"
+            redirect_to @user
+        else
+        	render 'new'
+        end
+    end
+
+    def edit
+        # @user defined in current_user before filter
+    end
+
+    def update
+        # @user defined in current_user before filter
+        if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+            sign_in @user
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
 
   	private
-  	  def user_params
+        def user_params
   		# This says that params[:department] is required, but inside that, only params[:department][:full_name] and 
   		# params[:department][:abbreviation] are permitted. Unpermitted params will be stripped out  		
 
   		  params.require(:user).permit!
   		end
+
+        # Before filters
+
+        def signed_in_user
+            unless signed_in?
+                ## store_locaiton <-- Method doesn't work because can't access the Sessions Helper?
+                redirect_to signin_url, notice: "Please sign in to access this page."
+            end
+        end
+
+        def correct_user
+            @user = User.find(params[:id])
+            redirect_to(root_path) unless current_user?(@user)
+        end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -38,7 +38,7 @@ describe "UserPages" do
 				fill_in "Last name",				with: "User"
 				fill_in "Email",					with: "user@example.gsi.gov.uk"
 				fill_in "Password",					with: "foobar"
-				fill_in "Confirmation",				with: "foobar"
+				fill_in "Confirm Password",				with: "foobar"
 			end
 
 			it "should create a user" do
@@ -54,5 +54,43 @@ describe "UserPages" do
 				it { should have_selector('div.alert.alert-success', text: "Welcome") }
 			end
 		end
+	end
+
+	describe "edit" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in user
+			visit edit_user_path(user)
+		end
+
+		describe "page" do
+			it { should have_content("Update your profile") }
+			it { should have_title("Edit user") }
+			it { should have_link('change', href: 'http://gravatar.com/emails') }
+		end
+
+		describe "with invalid information" do
+			before { click_button "Save changes" }
+
+			it { should have_content('error') }
+		end
+
+		describe "with valid information" do
+			let(:new_name)	{ "New" }
+			let(:new_email)	{ "new@justice.gsi.gov.uk" }
+			before do
+				fill_in "First name",			with: new_name
+				fill_in "Email",				with: new_email
+				fill_in "Password",				with: user.password
+				fill_in "Confirm Password",		with: user.password
+				click_button "Save changes"
+			end
+
+			it { should have_title(new_name) }
+      		it { should have_selector('div.alert.alert-success') }
+      		it { should have_link('Sign out', href: signout_path) }
+      		specify { expect(user.reload.first_name).to  eql(new_name) }
+      		specify { expect(user.reload.email).to eql(new_email) }
+      	end
 	end
 end
